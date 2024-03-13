6c889fe6f8f89e568337a09ee55fad70aafd84c4
Zahid Hasan Milu <minions.milu889@gmail.com>
2024-03-13 15:38:53 +0600

    initial commit

--- assetapp/urls.py
+++ assetapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from assetapp.views import CompanyListCreateView, EmployeeListCreateView, EmployeeDetails,AssetsList,Assets_Details
+from assetapp.views import CompanyListCreateView, EmployeeListCreateView, EmployeeDetails, AssetsList, Assets_Details, AssetsLogListCreateView, AssetsLogDetail
 
 
 urlpatterns = [
@@ -8,11 +8,16 @@ urlpatterns = [
     # These lines of code are defining employee URL patterns for the Django application.
     path('employee/', EmployeeListCreateView.as_view(), name='employee'),
     path('employee/<int:pk>/', EmployeeDetails.as_view(), name='employee_details'),
-    
-   # These lines of code are defining asset URL patterns for the Django application.
+
+    # These lines of code are defining asset URL patterns for the Django application.
     path('asset/', AssetsList.as_view(), name='assets'),
     path('asset/<int:pk>/', Assets_Details.as_view(), name='asset_details'),
-    
-    
+
+
+    # These lines of code are defining URL patterns for the asset log functionality in a Django application.
+    path('assetlog/', AssetsLogListCreateView.as_view(), name='assetlog'),
+    path('assetlog/<int:pk>/', AssetsLogDetail.as_view(), name='assetlog_details'),
+
+
 
 ]
